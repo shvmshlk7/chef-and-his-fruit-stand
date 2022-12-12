dc47d88f334bd375ff6202a68586e2357c019400
SHIVAM SHELKE <103065763+shvmshlk7@users.noreply.github.com>
2022-12-12 21:01:55 +0530

    Create FOODCHAAT.cpp

---
+++ FOODCHAAT.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while (t--)
+    {
+        int ans,x,y;
+        cin>>x>>y;
+        ans=x/2;
+        
+        if(ans>y)
+        {
+            cout<<y<<endl;
+        }
+        elsea
+        {
+            cout<<ans<<endl;
+        }
+        
+    }
+    return 0;
+}
